e0b0c3f7ed39dbbb55682d98efa409d032ef3653
ruzmuh <ruzmuh@gmail.com>
2021-12-01 16:11:35 +0500

    Initial commit

---
+++ deploy/policies.tf
@@ -0,0 +1,19 @@
+resource "azurerm_policy_definition" "allowed-location-definition" {
+  name         = "location-policy-definition"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name  = "eastus-location-policy"
+  policy_rule = <<POLICY_RULE
+    {
+    "if": {
+      "not": {
+        "field": "location",
+        "equals": "eastus"
+      }
+    },
+    "then": {
+      "effect": "deny"
+    }
+  }
+POLICY_RULE
+}
